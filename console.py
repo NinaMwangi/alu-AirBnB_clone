fa1e92f388b310b2f2008defeedda14312e05d01
NinaMwangi <n.mwangi@alustudent.com>
2023-09-07 11:33:31 +0300

    Adding console.py file

--- console.py
+++ console.py
@@ -4,3 +4,25 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """Hbnb Command Processor"""
+
+    prompt = '(hbnb)'
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program \n"""
+        return True
+    
+    def do_quit(self, arg):
+        """Quit command to exit the program \n"""
+        return True
+    
+    def do_help(self, arg: str):
+        """Describes what the methods do"""
+        return super().do_help(arg)
+    
+    def emptyline(self):
+        """Empty line + Enter doesnt execute"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
