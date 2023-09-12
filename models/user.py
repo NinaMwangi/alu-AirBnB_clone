8173f110acd4922b705eaa2fd880a3c55657b057
NinaMwangi <n.mwangi@alustudent.com>
2023-09-12 13:00:08 +0300

    User.py

--- models/user.py
+++ models/user.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-"""User class"""
-=======
 """Defines the User class."""
->>>>>>> a2667300aff346dc5092b189dcc5c7396bd5fceb
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-<<<<<<< HEAD
-    """User class"""
-=======
     """Represent a User.
 
     Attributes:
@@ -19,16 +12,12 @@ class User(BaseModel):
         first_name (str): The first name of the user.
         last_name (str): The last name of the user.
     """
-
->>>>>>> a2667300aff346dc5092b189dcc5c7396bd5fceb
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-<<<<<<< HEAD
 
     def __init__(self, *args, **kwargs):
         """initialise class"""
         super().__init__(*args, **kwargs)
-=======
->>>>>>> a2667300aff346dc5092b189dcc5c7396bd5fceb
+
